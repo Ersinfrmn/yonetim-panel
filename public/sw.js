0326fae1854127d4c36f5b67c243b4408df35d3c
Ersin FERMAN <ersinfermann@gmail.com>
2026-06-27 19:20:42 +0300

    auto: update

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'ef-komuta-v1'
+const STATIC_ASSETS = ['/', '/index.html']
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return
+  if (e.request.url.includes('supabase')) return
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  )
+})
